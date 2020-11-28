e819f2c32db3f0db68ccae5e450d915b0e70aacd
sivasankarLM10 <73575074+sivasankarLM10@users.noreply.github.com>
2020-11-28 21:45:15 +0530

    task-8.json
    task-8.json

---
+++ perceval_github_1.py
@@ -0,0 +1,28 @@
+import argparse
+
+from perceval.backends.core.github import GitHub
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description = "Simple parser for GitHub issues and pull requests"
+    )
+parser.add_argument("-t", "--token",
+                    '--nargs', nargs='+',
+                    help = "GitHub token")
+parser.add_argument("-r", "--repo",
+                    help = "GitHub repository, as 'owner/repo'")
+args = parser.parse_args()
+
+# Owner and repository names
+(owner, repo) = args.repo.split('/')
+
+# create a Git object, pointing to repo_url, using repo_dir for cloning
+repo = GitHub(owner=owner, repository=repo, api_token=args.token)
+# fetch all issues/pull requests as an iterator, and iterate it printing
+# their number, and whether they are issues or pull requests
+for item in repo.fetch():
+    if 'pull_request' in item['data']:
+        kind = 'Pull request'
+    else:
+        kind = 'Issue'
+    print(item['data']['number'], ':', kind)
